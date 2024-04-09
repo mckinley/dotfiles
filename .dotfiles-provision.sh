0deb663d5060b41c850f0f1cb6ba27a2f61a574b
Bronson McKinley <mckinley@users.noreply.github.com>
2024-04-08 17:15:34 -0700

    add dependencies for ruby-build when provisioning

--- .dotfiles-provision.sh
+++ .dotfiles-provision.sh
@@ -25,6 +25,10 @@ brew install \
   direnv \
   asdf
 
+# dependencies for ruby-build which is used by asdf-ruby
+# https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
+brew install openssl@1.1 openssl@3 readline libyaml gmp
+
 echo "- Installing all asdf managed tools with Homebrew to ensure that dependencies are installed"
 cut -d' ' -f1 .tool-versions | xargs -tI{} brew install {}
 
